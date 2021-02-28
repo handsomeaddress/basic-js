e16c379d6057f8d31a681008c1059ecc3e4c8a2c
handsomeaddress <yeiehahaha@gmail.com>
2021-02-28 20:49:54 +0300

    feat: implement Chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+	chain: [],
+	getLength() {
+		return this.chain.length;
+	},
+	addLink(value) {
+		this.chain.push(String(value));
+		return this;
+	},
+	removeLink(position) {
+		if(typeof position === 'number' && position > 0 && position <= this.chain.length - 1){
+			this.chain.splice(position - 1, 1);
+			return this;
+		}else{
+			this.chain = [];
+			throw new Error;
+		}
+	},
+	reverseChain() {
+		this.chain.reverse();
+		return this;
+	},
+	finishChain() {
+		let result = this.chain;
+		this.chain = [];
+		return '( ' + result.join(' )~~( ') + ' )';
+	}
 };
 
 module.exports = chainMaker;
