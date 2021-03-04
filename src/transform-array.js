226596afc29283579c887e4ca316902016f9e60f
handsomeaddress <yeiehahaha@gmail.com>
2021-03-04 19:52:20 +0300

    feat: add implement  Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,52 @@
 const CustomError = require("../extensions/custom-error");
+const command = [
+	'--discard-next',
+	'--discard-prev',
+	'--double-next',
+	'--double-prev'
+];
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr){
+	if (!Array.isArray(arr)) {
+        throw new Error('Error');
+    }
+	let nArr = [],
+		next = 0;
+	arr.forEach((el,index) => {
+		if(next > 0){
+			next = 0;
+			return true;
+		}
+		switch(el){
+			case command[0]:
+				next++;
+				break;
+			case command[1]:
+				if (arr[index - 2] !== command[0]){
+					index--;
+					nArr.pop();
+				}
+				break;
+			case command[2]:
+				index++;
+				index < arr.length - 1 ? nArr.push(arr[index]) : false;
+				break;
+			case command[3]:
+				if(index-1 > 0){
+					if(arr[index-2] != command[0]){
+						nArr.push(arr[index-1]);
+					}else{
+						return true;
+					}
+				}else{
+					return true;
+				}
+				break;
+			default:
+				nArr.push(el);
+				true;
+			break;
+		}
+	});
+	return nArr;
 };
